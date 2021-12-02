6ededa38699199226bfb8a735219a7e95ffc0490
Daniel Roe <daniel@roe.dev>
2021-12-02 10:20:44 +0000

    chore: initial commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,20 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  css: ['~/assets/main.css'],
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
-};
+  buildModules: ['@nuxtjs/pwa'],
+
+  pwa: {
+    meta: {
+      name: 'Building a PWA with Nuxt',
+      theme_color: '#00DC82',
+    },
+    manifest: {
+      name: 'Building a PWA with Nuxt',
+      short_name: 'Nuxt PWA',
+    },
+    // workbox
+  },
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    We're building a PWA with Nuxt at the eCommerce Tech Summit!
+    <header><h1>Building a PWA with Nuxt</h1></header>
+    <main>
+      <p>We're building a PWA with Nuxt at the eCommerce Tech Summit!</p>
+      <img :src="$icon(384)" height="192" width="192" alt="Nuxt logo" />
+    </main>
   </div>
 </template>
